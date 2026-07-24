60a82a83dc577ae0ce52989066c219d813c06f17
shaochuyu <shaochuyu@360.cn>
2026-07-24 21:26:37 +0800

    feat(bruteforce): 新增bruteforce插件

--- core/plugins/bruteforce/bruteforce_finger_community.go
+++ core/plugins/bruteforce/bruteforce_finger_community.go
@@ -3,3 +3,19 @@
 * @Date: 5/7/2022 11:30
  */
 package bruteforce
+
+import (
+	"context"
+	"wscan/core/plugins/base"
+)
+
+type PluginType int
+
+func (*PluginType) GetConfig(ctx context.Context, a *base.Apollo) *PluginConfig {
+
+	return nil
+}
+
+func (*dictionaryLoader) load() {
+
+}
