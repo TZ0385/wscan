676b926ad171152aa102e7d6e3599f23b616106b
shaochuyu <shaochuyu@360.cn>
2026-07-24 21:41:24 +0800

    feat(helper): 新增helper

--- core/plugins/helper/expr/RandInt.go
+++ core/plugins/helper/expr/RandInt.go
@@ -2,4 +2,4 @@
 * @Author: shaochuyu
 * @Date: 5/7/2022 11:30
  */
-package geodb
+package expr
--- core/plugins/helper/expr/element.go
+++ core/plugins/helper/expr/element.go
@@ -3,3 +3,8 @@
 * @Date: 5/7/2022 11:30
  */
 package expr
+
+type Element interface {
+	GetPosition() int
+	Value(string) (string, error)
+}
